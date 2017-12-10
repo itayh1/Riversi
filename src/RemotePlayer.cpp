b5b365c44039d98401f76cb1bf6db4cdb8348f5e
Itayh1 <itayh.msn@hotmail.com>
2017-12-10 04:03:55 +0200

    fix client and complete some functions

--- src/RemotePlayer.cpp
+++ src/RemotePlayer.cpp
@@ -4,15 +4,8 @@
 
 #include "../headers/RemotePlayer.h"
 
-RemotePlayer::RemotePlayer(int sign) : Player(sign) {
-    this->client = new Client("127.0.0.1", 9090);
-    try {
-        this->client->connectToServer();
-    } catch (const char *msg) {
-        cout << "Failed to connect to server. Reason: " << msg << endl;
-        exit(-1);
-    }
-
+RemotePlayer::RemotePlayer(Client *cl, int sign) : Player(sign) {
+    this->client = cl;
 }
 
 int RemotePlayer::getSign() {
@@ -20,10 +13,17 @@ int RemotePlayer::getSign() {
 }
 
 Point RemotePlayer::getPoint(vector<Path> paths, Board board) {
-    return Point();
+    char buffer[512];
+    int x, y;
+    this->client->readFromServer(buffer);
+    string str(buffer);
+    istringstream(str.substr(0,1)) >> x;
+    istringstream(str.substr(2,1)) >> y;
+    return Point(x,y);
 }
 
-void RemotePlayer::printMovePlayed(Point &p) {
+
+void RemotePlayer::movePlayed(Point &p) {
     char ch = sign == black ? 'X' : 'O';
     cout << ch << " played ";
     p.PrintPoint();
